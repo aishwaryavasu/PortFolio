c37cc81ae6ede822dae8234ae032a5aa4286f111
aishwaryavasu <aishwaryavasu1997@gmail.com>
2018-08-10 11:09:50 +0530

    Initial commit

---
+++ src/PortFolio.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class PortFolio {
+
+    ArrayList<Row> records = new ArrayList<>();
+
+    public void computeRowList(){
+
+    }
+
+    public void squashList(){
+        //update records with the squashed list
+    }
+}
---
+++ src/Row.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Row {
+    int start;
+    int end;
+    String statusCode;
+    String transferCode;
+
+    public Row(int start, int end, String statusCode, String transferCode) {
+        this.start = start;
+        this.end = end;
+        this.statusCode = statusCode;
+        this.transferCode = transferCode;
+    }
+
+    public List<Row> modifyRow(Row input){
+        return null;
+    }
+}
